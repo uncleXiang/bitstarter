4a71b7f9c75571a398f57b6005a4deaaa98431bb
Xiang Li <lixiang.cmu@gmail.com>
2013-07-13 18:16:04 +0900

    read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var s = fs.readFileSync("index.html");
+  response.send(s.toString());
 });
 
 var port = process.env.PORT || 5000;
